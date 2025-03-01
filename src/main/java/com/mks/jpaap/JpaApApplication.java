c2453745374a5caf9a9f6db95146671ac039d99d
KHARY_SEYE <mamekharyseye380@gmail.com>
2025-03-01 16:16:06 +0000

    creation du projet

---
+++ src/main/java/com/mks/jpaap/JpaApApplication.java
@@ -0,0 +1,18 @@
+package com.mks.jpaap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
